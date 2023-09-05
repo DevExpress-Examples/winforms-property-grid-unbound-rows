9b565c0ba024f706e99812c6d4cea22d931a41c3
DevExpressExampleBot <fakeEmail>
2023-09-05 12:08:52 +0400

    Source auto update [skip ci]

--- VB/DXApplication3/CustomDescriptor/UnboundRowHelper.vb
+++ VB/DXApplication3/CustomDescriptor/UnboundRowHelper.vb
@@ -1,17 +1,17 @@
-﻿Imports System
+Imports System
 Imports System.Collections.Generic
-Imports System.ComponentModel
-Imports System.Collections
 Imports DevExpress.XtraVerticalGrid
 Imports DevExpress.XtraVerticalGrid.Events
+Imports System.ComponentModel
+Imports System.Collections
 
-Namespace WindowsApplication1
+Namespace DXApplication3
+
     Public Class UnboundRowsHelper
-
 
         Private ReadOnly _PropertyGrid As PropertyGridControl
 
-        Private _UnboundRows As New List(Of UnboundRow)()
+        Private _UnboundRows As List(Of UnboundRow) = New List(Of UnboundRow)()
 
         Public Sub New(ByVal pg As PropertyGridControl)
             _PropertyGrid = pg
@@ -19,18 +19,18 @@ Namespace WindowsApplication1
         End Sub
 
         Private Sub _PropertyGrid_CustomPropertyDescriptors(ByVal sender As Object, ByVal e As CustomPropertyDescriptorsEventArgs)
-            If (TryCast(sender, PropertyGridControl)).SelectedObject = e.Source Then
+            If TryCast(sender, PropertyGridControl).SelectedObject Is e.Source Then
                 Dim properties As PropertyDescriptorCollection = e.Properties
-                Dim list As New ArrayList(properties)
+                Dim list As ArrayList = New ArrayList(properties)
                 list.AddRange(_UnboundRows)
-                Dim result(list.Count - 1) As PropertyDescriptor
+                Dim result As PropertyDescriptor() = New PropertyDescriptor(list.Count - 1) {}
                 list.ToArray().CopyTo(result, 0)
                 e.Properties = New PropertyDescriptorCollection(result)
             End If
         End Sub
 
         Public Sub AddUnboundRow(ByVal fieldName As String, ByVal valueType As Type)
-            Dim row As New UnboundRow(fieldName, valueType)
+            Dim row As UnboundRow = New UnboundRow(fieldName, valueType)
             _UnboundRows.Add(row)
         End Sub
     End Class
--- VB/DXApplication3/Form1.Designer.vb
+++ VB/DXApplication3/Form1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace WindowsApplication1
-    Partial Public Class Form1
+Namespace DXApplication3
+
+    Partial Class Form1
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
@@ -10,14 +12,14 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Windows Form Designer generated code"
-
+'#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
@@ -27,36 +29,34 @@
             Me.splitContainerControl1 = New DevExpress.XtraEditors.SplitContainerControl()
             Me.propertyGridControl1 = New DevExpress.XtraVerticalGrid.PropertyGridControl()
             Me.propertyDescriptionControl1 = New DevExpress.XtraVerticalGrid.PropertyDescriptionControl()
-            DirectCast(Me.panelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.panelControl1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.panelControl1.SuspendLayout()
-            DirectCast(Me.splitContainerControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.splitContainerControl1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.splitContainerControl1.SuspendLayout()
-            DirectCast(Me.propertyGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.propertyGridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
             ' 
             ' panelControl1
             ' 
             Me.panelControl1.Controls.Add(Me.splitContainerControl1)
             Me.panelControl1.Dock = System.Windows.Forms.DockStyle.Right
-            Me.panelControl1.Location = New System.Drawing.Point(592, 0)
-            Me.panelControl1.Margin = New System.Windows.Forms.Padding(4)
+            Me.panelControl1.Location = New System.Drawing.Point(219, 0)
             Me.panelControl1.Name = "panelControl1"
-            Me.panelControl1.Size = New System.Drawing.Size(312, 656)
-            Me.panelControl1.TabIndex = 2
+            Me.panelControl1.Size = New System.Drawing.Size(200, 448)
+            Me.panelControl1.TabIndex = 0
             ' 
             ' splitContainerControl1
             ' 
             Me.splitContainerControl1.Dock = System.Windows.Forms.DockStyle.Fill
             Me.splitContainerControl1.Horizontal = False
             Me.splitContainerControl1.Location = New System.Drawing.Point(2, 2)
-            Me.splitContainerControl1.Margin = New System.Windows.Forms.Padding(4)
             Me.splitContainerControl1.Name = "splitContainerControl1"
             Me.splitContainerControl1.Panel1.Controls.Add(Me.propertyGridControl1)
             Me.splitContainerControl1.Panel1.Text = "Panel1"
             Me.splitContainerControl1.Panel2.Controls.Add(Me.propertyDescriptionControl1)
             Me.splitContainerControl1.Panel2.Text = "Panel2"
-            Me.splitContainerControl1.Size = New System.Drawing.Size(308, 652)
-            Me.splitContainerControl1.SplitterPosition = 470
+            Me.splitContainerControl1.Size = New System.Drawing.Size(196, 444)
+            Me.splitContainerControl1.SplitterPosition = 326
             Me.splitContainerControl1.TabIndex = 0
             Me.splitContainerControl1.Text = "splitContainerControl1"
             ' 
@@ -64,48 +64,44 @@
             ' 
             Me.propertyGridControl1.Dock = System.Windows.Forms.DockStyle.Fill
             Me.propertyGridControl1.Location = New System.Drawing.Point(0, 0)
-            Me.propertyGridControl1.Margin = New System.Windows.Forms.Padding(4)
             Me.propertyGridControl1.Name = "propertyGridControl1"
             Me.propertyGridControl1.SelectedObject = Me
-            Me.propertyGridControl1.Size = New System.Drawing.Size(308, 470)
-            Me.propertyGridControl1.TabIndex = 5
+            Me.propertyGridControl1.Size = New System.Drawing.Size(196, 326)
+            Me.propertyGridControl1.TabIndex = 1
             ' 
             ' propertyDescriptionControl1
             ' 
             Me.propertyDescriptionControl1.Dock = System.Windows.Forms.DockStyle.Fill
             Me.propertyDescriptionControl1.Location = New System.Drawing.Point(0, 0)
-            Me.propertyDescriptionControl1.Margin = New System.Windows.Forms.Padding(4)
             Me.propertyDescriptionControl1.Name = "propertyDescriptionControl1"
             Me.propertyDescriptionControl1.PropertyGrid = Me.propertyGridControl1
-            Me.propertyDescriptionControl1.Size = New System.Drawing.Size(308, 174)
-            Me.propertyDescriptionControl1.TabIndex = 5
+            Me.propertyDescriptionControl1.Size = New System.Drawing.Size(196, 113)
+            Me.propertyDescriptionControl1.TabIndex = 2
             Me.propertyDescriptionControl1.TabStop = False
             ' 
             ' Form1
             ' 
-            Me.AutoScaleDimensions = New System.Drawing.SizeF(8F, 16F)
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
             Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-            Me.ClientSize = New System.Drawing.Size(904, 656)
+            Me.ClientSize = New System.Drawing.Size(419, 448)
             Me.Controls.Add(Me.panelControl1)
-            Me.Margin = New System.Windows.Forms.Padding(4)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            DirectCast(Me.panelControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.panelControl1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.panelControl1.ResumeLayout(False)
-            DirectCast(Me.splitContainerControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.splitContainerControl1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.splitContainerControl1.ResumeLayout(False)
-            DirectCast(Me.propertyGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.propertyGridControl1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
-
         End Sub
 
-        #End Region
-
+'#End Region
         Private panelControl1 As DevExpress.XtraEditors.PanelControl
-        Private splitContainerControl1 As DevExpress.XtraEditors.SplitContainerControl
-        Private propertyGridControl1 As DevExpress.XtraVerticalGrid.PropertyGridControl
-        Private propertyDescriptionControl1 As DevExpress.XtraVerticalGrid.PropertyDescriptionControl
 
+        Private splitContainerControl1 As DevExpress.XtraEditors.SplitContainerControl
+
+        Private propertyGridControl1 As DevExpress.XtraVerticalGrid.PropertyGridControl
+
+        Private propertyDescriptionControl1 As DevExpress.XtraVerticalGrid.PropertyDescriptionControl
     End Class
 End Namespace
-
